c4aff180fbe8aac1ccf4a0fcd22667d761127c67
Jose <armasjose1980@gmail.com>
2019-10-06 00:22:02 +0200

    Create Tables Postgre Ibaiondo

---
+++ AppActivGest/src/controlador/postgre/CreateTablesPostgreIbaiondo.java
@@ -0,0 +1,262 @@
+package controlador.postgre;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class CreateTablesPostgreIbaiondo {
+
+
+    public static void main(String[] args) throws SQLException {
+
+        Connection c = conectorBd();
+
+        crearTablas(c);
+
+        insertarEmpleados(c);
+
+        insertarActividades(c);
+
+        insertarUsuarios(c);
+
+
+    }
+
+    private static Connection conectorBd() {
+
+        String url = "jdbc:postgresql://localhost:5432/Ibaiondo";
+        String user = "postgres";
+        String passw = "abcde";
+        Connection con = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+             con = DriverManager.getConnection(url,user,passw);
+        } catch (SQLException e) {
+            javax.swing.JOptionPane.showMessageDialog(null ,"Ha ocurrido un problema \n"+e.getMessage());
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            javax.swing.JOptionPane.showMessageDialog(null ,"Ha ocurrido un problema \n"+e.getMessage());
+            e.printStackTrace();
+        }
+
+
+        return con;
+    }
+
+    private static void crearTablas(Connection conexion) {
+
+        String createEmpleados = "CREATE TABLE EMPLEADOS(" +
+                "DNI VARCHAR(255) PRIMARY KEY NOT NULL," +
+                "NOMBRE VARCHAR(255) NOT NULL," +
+                "APELLIDO1 VARCHAR(255)," +
+                "APELLIDO2 VARCHAR(255)," +
+                "FECHACONTRACT VARCHAR(10)," +
+                "FECHANAC VARCHAR(10)," +
+                "NACIONALIDAD VARCHAR(20)," +
+                "CARGO VARCHAR(255));";
+
+
+        String createActividades = "CREATE TABLE ACTIVIDADES(" +
+                "NUMACTIVIDAD VARCHAR(255) PRIMARY KEY NOT NULL," +
+                "NOMBRE VARCHAR(255) NOT NULL," +
+                "NUMEROMAXIMOINVITADO INT NOT NULL," +
+                "NOMBRESALA VARCHAR(20)," +
+                "CURSOACADEMICO VARCHAR (20)," +
+                "COSTE FLOAT," +
+                "DNIEMPLEADO VARCHAR(255)," +
+                "FOREIGN  KEY (DNIEMPLEADO) REFERENCES  EMPLEADOS(DNI));";
+
+        //PROBLEMA CON TABLAS DNI Usuario sin tabla creada
+        String createUsuarios = "CREATE TABLE USUARIOS(" +
+                "DNI VARCHAR(9) PRIMARY KEY NOT NULL," +
+                "NOMBRE VARCHAR(255) NOT NULL," +
+                "APELLIDO1 VARCHAR(255)," +
+                "APELLIDO2 VARCHAR(255)," +
+                "EDAD INT," +
+                "PROFESION VARCHAR (20));";
+
+        String createSesion = "CREATE TABLE SESION(" +
+                "IDSESION SERIAL PRIMARY KEY ," +
+                "HORA VARCHAR(5)," +
+                "DIASEMANA VARCHAR(255)," +
+                "NUMACTIVIDAD VARCHAR(255)," +
+                "DNIUSUARIO VARCHAR(9)," +
+                "FOREIGN KEY (NUMACTIVIDAD) REFERENCES ACTIVIDADES(NUMACTIVIDAD)," +
+                "FOREIGN KEY (DNIUSUARIO) REFERENCES  USUARIOS(DNI));";
+
+        //NOTAS:
+        //DOS TIPOS DE FECHA LAS DE DÍAS TENDRÁN ÉSTE FORMATO: 25-04-2019 (10 DÍGITOS)
+        //Y LAS DE HORAS TENDRÁN ÉSTE OTRO: 13:45 (5 DÍGITOS)
+        try {
+            PreparedStatement pst;
+            try {
+                System.out.println("*** Creando tabla empleados ***");
+
+                assert conexion != null;
+                pst = conexion.prepareStatement(createEmpleados);
+                pst.executeUpdate();
+                pst.close();
+
+                System.out.println("*** Tabla empleados creada correctamente ***");
+            } catch (Exception e) {
+                System.out.println("*** Tabla empleados creada INCORRECTAMENTE ***");
+                System.out.println(e.getMessage());
+            }
+
+            try {
+                System.out.println("*** Creando tabla actividades ***");
+
+                assert conexion != null;
+                pst = conexion.prepareStatement(createActividades);
+                pst.executeUpdate();
+                pst.close();
+
+                System.out.println("*** Tabla actividades creada correctamente ***");
+            } catch (Exception e) {
+                System.out.println("*** Tabla actividades creada INCORRECTAMENTE ***");
+                System.out.println(e.getMessage());
+            }
+
+            try {
+                System.out.println("*** Creando tabla usuarios ***");
+
+                assert conexion != null;
+                pst = conexion.prepareStatement(createUsuarios);
+                pst.executeUpdate();
+                pst.close();
+
+                System.out.println("*** Tabla usuarios creada correctamente ***");
+            } catch (Exception e) {
+                System.out.println("*** Tabla usuarios creada INCORRECTAMENTE ***");
+                System.out.println(e.getMessage());
+            }
+
+            try {
+                System.out.println("*** Creando tabla sesion ***");
+
+                assert conexion != null;
+                pst = conexion.prepareStatement(createSesion);
+                pst.executeUpdate();
+                pst.close();
+
+                System.out.println("*** Tabla sesion creada correctamente ***");
+            } catch (Exception e) {
+                System.out.println("*** Tabla sesion creada INCORRECTAMENTE ***");
+                System.out.println(e.getMessage());
+            }
+
+            System.out.println("*** PROCESO DE CREACIÓN DE TABLAS FINALIZADO ****");
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void insertarActividades(Connection con) {
+
+        try {
+
+            String query = "INSERT INTO actividades (numactividad,nombre,numeromaximoinvitado,"
+                    + "nombresala,cursoacademico,coste) VALUES(?,?,?,?,?,?)";
+
+            java.sql.Statement statement = con.createStatement();
+
+            PreparedStatement ps;
+            ps = con.prepareStatement(query);
+
+
+            ps.setString(1, "1");
+            ps.setString(2, "Padel");
+            ps.setInt(3, 10);
+            ps.setString(4, "Sala padel");
+            ps.setString(5, "2018/2019");
+            ps.setDouble(6, 20);
+
+
+            ps.execute();
+            ps.close();
+
+            System.out.println("Datos insertados en actividades correctamente");
+
+
+        } catch (SQLException e) {
+
+            System.out.println("Error en la insercción de datos...");
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private static void insertarUsuarios(Connection con) {
+
+        try {
+
+            String query = "INSERT INTO usuarios VALUES(?,?,?,?,?,?)";
+
+            PreparedStatement ps;
+            ps = con.prepareStatement(query);
+
+            ps.setString(1, "44686144L");
+            ps.setString(2, "Jose");
+            ps.setString(3, "Armas");
+            ps.setString(4, "López ");
+            ps.setInt(5, 39);
+            ps.setString(6, "conserje");
+
+            ps.execute();
+            ps.close();
+
+            System.out.println("Datos insertados  en usuarios correctamente");
+
+
+        } catch (SQLException e) {
+
+            System.out.println("Error en la insercción de datos...");
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void insertarEmpleados(Connection con) {
+
+        try {
+
+            String query = "INSERT INTO empleados VALUES (?,?,?,?,?,?,?,?)";
+
+            PreparedStatement ps = con.prepareStatement(query);
+
+            ps.setString(1, "1");
+            ps.setString(2, "Ander");
+            ps.setString(3, "Pérez");
+            ps.setString(4, "Pérez ");
+            ps.setString(5, "01/10/2019");
+            ps.setString(6, "04/02/1980");
+            ps.setString(7,"Española");
+            ps.setString(8, "Conserje");
+
+            int row = ps.executeUpdate();
+
+            // rows affected
+            //System.out.println(row); //1
+
+            ps.close();
+
+            System.out.println("Datos insertados en empleados correctamente");
+
+
+        } catch (SQLException e) {
+
+            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+            e.printStackTrace();
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
+
+
+    }
+}
