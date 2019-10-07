e2a612c2bd2831071243abb15b83547227166740
Jose <armasjose1980@gmail.com>
2019-10-08 00:01:29 +0200

    Carga de tablas de Ibaiondo
    Se cargan las tablas pero no me deja eliminar empleados, usuarios ni empleados.
    Error de datos en las tablas de Postgre.

--- AppActivGest/src/vista/VentanaTablesAcUsEm.java
+++ AppActivGest/src/vista/VentanaTablesAcUsEm.java
@@ -20,7 +20,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.util.List;
 
-public class VentanaTablesAcUsEm  {
+public class VentanaTablesAcUsEm {
 
     private JPanel ventanaTablesAcUsEmJpanel;
     private JTable tableAcUsEm;
@@ -47,33 +47,30 @@ public class VentanaTablesAcUsEm  {
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
 
-        switch (cc){
+        switch (cc) {
 
-            case("Hegoalde"):
+            case ("Hegoalde"):
 
                 cargarUsuariosActividadesEmpleadosSqliteHegoalde(cc);
                 cargarDatosEnTabla(tipo);
 
                 break;
-            case("Iparralde"):
+            case ("Iparralde"):
 
                 cargarDatosEnTabla(tipo);
                 break;
 
-            case("Arriaga"):
+            case ("Arriaga"):
                 CargarUsuariosActividadesEmpleadosMySQLArriaga();
                 cargarDatosEnTabla(tipo);
 
                 break;
-            case("Ibaiondo"):
+            case ("Ibaiondo"):
 
-                //codigo eric
-               //cargarDatosEnTabla(tipo);
+                cargarUsuariosActividadesEmpleadosSqliteHegoalde(cc);
+                cargarDatosEnTabla(tipo);
                 break;
         }
-
-
-
 
 
         nuevaButton.addActionListener(new ActionListener() {
@@ -92,26 +89,26 @@ public class VentanaTablesAcUsEm  {
         });
     }
 
-    public void cargarDatosEnTabla(String tipo){
-        switch (tipo){
+    public void cargarDatosEnTabla(String tipo) {
+        switch (tipo) {
             case "Actividades":
-                if(actividades.size() > 0){
+                if (actividades.size() > 0) {
                     tableAcUsEm.setModel(new ActividadesTableModel(actividades));
                 } else {
                     javax.swing.JOptionPane.showMessageDialog(null, "List actividades vacío!");
                 }
                 break;
             case "Usuarios":
-                if(usuarios.size() > 0){
+                if (usuarios.size() > 0) {
                     tableAcUsEm.setModel(new UsuariosTableModel(usuarios));
-                }else {
+                } else {
                     javax.swing.JOptionPane.showMessageDialog(null, "List usuario vacío!");
                 }
                 break;
             case "Empleados":
-                if(empleados.size() > 0){
+                if (empleados.size() > 0) {
                     tableAcUsEm.setModel(new EmpleadosTableModel(empleados));
-                }else {
+                } else {
                     javax.swing.JOptionPane.showMessageDialog(null, "List empleados vacío!");
                 }
                 break;
@@ -119,7 +116,7 @@ public class VentanaTablesAcUsEm  {
     }
 
     // funcion que devuelve los datos de la bd a usuarios, actividades, empleados
-    public void cargarUsuariosActividadesEmpleadosSqliteHegoalde(String cc){
+    public void cargarUsuariosActividadesEmpleadosSqliteHegoalde(String cc) {
 
         ControladorBbDd controladorBbDd = new ControladorBbDd(cc);
         SqliteConsulta sqliteConsulta = new SqliteConsulta(controladorBbDd.getConexion());
@@ -133,23 +130,22 @@ public class VentanaTablesAcUsEm  {
         usuarios = sqliteConsulta.getUsuarios();
 
 
-
     }
 
 
-    public void CargarUsuariosActividadesEmpleadosMySQLArriaga(){
+    public void CargarUsuariosActividadesEmpleadosMySQLArriaga() {
 
         Connection con = null;
 
-        try{
+        try {
             Class.forName("com.mysql.jdbc.Driver");
             con = DriverManager.getConnection("jdbc:mysql://localhost:3306/actigest", "root", "");
 
-        }catch (Exception e) {
-            System.err.println("No se ha podido conectar a la base de datos Arriaga\n"+e.getMessage());
+        } catch (Exception e) {
+            System.err.println("No se ha podido conectar a la base de datos Arriaga\n" + e.getMessage());
         }
 
-        MysqlConsultas  mysqlConsultas = new MysqlConsultas(con);
+        MysqlConsultas mysqlConsultas = new MysqlConsultas(con);
         empleados = mysqlConsultas.LeerEmpleadosArriaga();
         usuarios = mysqlConsultas.LeerUsuariosArriaga();
         actividades = mysqlConsultas.LeerActividadesArriaga();
